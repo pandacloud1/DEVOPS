0a2e67e9b26a567b01f7121028e055de75800ca7
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-02-08 17:23:18 +0530

    Create ansible.bash

---
+++ Installation_Scripts/Ansible/Amazon_Linux2/ansible.bash
@@ -0,0 +1,3 @@
+sudo yum update -y
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+sudo yum install git python python-devel python-pip openssl ansible -y
