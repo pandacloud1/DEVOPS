2b30756a8d08372d15fbd28227501ed4e7bc90f1
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-02-08 17:23:57 +0530

    Create docker.bash

---
+++ Installation_Scripts/Docker/Amazon_Linux2/docker.bash
@@ -0,0 +1,8 @@
+# REF: https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
+#!/bin/bash
+sudo yum update -y
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+# Add ec2-user to the docker group so that Docker commands can be run w/o using sudo.
+sudo usermod -a -G docker ec2-user
