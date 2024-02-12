13e02e4091808a10f2f69d4967acf90f23f5eecd
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-02-12 21:31:59 +0530

    Update docker.bash

--- Installation_Scripts/Docker/Amazon_Linux2/docker.bash
+++ Installation_Scripts/Docker/Amazon_Linux2/docker.bash
@@ -7,4 +7,6 @@ sudo systemctl enable docker
 # Add ec2-user to the docker group so that Docker commands can be run w/o using sudo.
 sudo usermod -aG docker $USER && newgrp docker  #(add normal user 'docker')
 sudo usermod -aG docker ec2-user
+# To avoid below permission error
+# Got permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock
 sudo chmod 777 /var/run/docker.sock
