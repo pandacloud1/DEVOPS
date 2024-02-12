2741f3c6f704244d7b5a39c578738d932ef8fed3
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-02-12 21:30:59 +0530

    Update jenkins.bash

--- Installation_Scripts/Jenkins/Amazon_Linux/jenkins.bash
+++ Installation_Scripts/Jenkins/Amazon_Linux/jenkins.bash
@@ -1,3 +1,6 @@
+#!/bin/bash
+# REF: (https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/)
+
 sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
