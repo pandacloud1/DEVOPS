eea4e2937593f12c6585ad9adc9e494a8d0100d8
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-02-12 21:34:32 +0530

    Create trivy.bash

---
+++ Installation_Scripts/Trivy/Amazon_Linux2/trivy.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+# REF: https://aquasecurity.github.io/trivy/v0.18.3/installation/
+sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm
