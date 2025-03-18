df952ef1d5e4666f89eb7259a6c242dfeebd1d9a
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2025-03-18 13:46:13 +0530

    Update main.tf

--- JENKINS/installation/main.tf
+++ JENKINS/installation/main.tf
@@ -73,7 +73,7 @@ resource "aws_security_group" "my-sg" {
 resource "aws_instance" "my-ec2" {
   ami = "ami-0a0e5d9c7acc336f1"    # Ubuntu 22.04 (us-east-1)
   instance_type = "t2.micro"
-  key_name = "Linux-Key"           # Replace with your key-name
+  key_name = "Linux_key"           # Replace with your key-name
   vpc_security_group_ids = [aws_security_group.my-sg.id]
 
   tags = {
