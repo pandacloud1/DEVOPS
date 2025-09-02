b6a06aac754251a2de426ca4dab53962d8d98265
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2025-09-02 16:30:59 +0530

    Update main.tf

--- Terraform/modules/module2/main.tf
+++ Terraform/modules/module2/main.tf
@@ -1,65 +1,15 @@
-# Create Security Group
-resource "aws_security_group" "my_sg" {
-  name = "Apache-Security-Group"
-  ingress {
-    from_port = 22
-    to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol = "tcp"
-  }
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol = "tcp"
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol = "tcp"
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol = "ALL"
-  }
-
+provider "aws" {
+  region = "us-east-1"
 }
 
-# CREATE EC2 INSTANCE
-resource "aws_instance" "my-ec2" {
-  ami = var.ami
-  instance_type = var.instance_type
-  key_name = var.key_name
-  vpc_security_group_ids =  [aws_security_group.my_sg.id]
+module "vpc" {
+  source       = "./modules/vpc"
+}
 
-  provisioner "local-exec" {
-    command = "echo WE ARE SETTING UP APACHE WEBSERVER"
-  }
+module "ec2" {
+  source         = "./modules/ec2"
+}
 
-  provisioner "remote-exec" {
-    connection { 
-      type = "ssh"
-      private_key = file("./Linux_key.pem")
-      user = var.user
-      host = aws_instance.my-ec2.public_ip
-    }
-
-    inline = [
-      "sudo yum update -y",
-      "sudo yum install httpd -y",
-      "sudo systemctl start httpd",
-      "sudo chkconfig httpd on",
-      "sudo echo 'THIS IS MY WEBSERVER' | sudo tee /var/www/html/index.html"
-    ]
-  }
-
-  tags = {
-     Name = var.tags
-  }
+module "s3" {
+  source      = "./modules/s3"
 }
