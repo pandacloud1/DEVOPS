5943346a980317c19a73f12b418901d5dffc15bf
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2025-09-02 16:52:44 +0530

    Create main.tf

---
+++ Terraform/modules/module2/modules/vpc/main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.az
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-subnet"
+  }
+}
